0daca1db39bed1a0265efc643412074023c90e34
danjrbravo <danjrbrav0@gmail.com>
2026-04-25 20:08:43 -0500

    Added footer component to Home

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -2,12 +2,15 @@ import React from 'react'
 import Navbar from '../navbar/Navbar'
 import Herobanner from '../herobanner/Herobanner'
 import HourFrontServices from '../frontservicios/HourFrontServices'
+import Footer from '../footer/Footer'
+
 const Home = () => {
   return (
     <>
     <Navbar></Navbar>
     <Herobanner></Herobanner>
     <HourFrontServices></HourFrontServices>
+    <Footer></Footer>
     </>
   )
 }
---
+++ src/footer/Footer.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import './Footer.css'
+const Footer = () => {
+  return (
+    <>
+    <footer className='footer'>
+        <div className='footer-left'>
+            <h3>Grupo aguapanelita</h3>
+            <p>Lavado profesional de vehiculos</p>
+        </div>
+        <div className='footer-mid'>
+            <h4>Politicas</h4>
+            <a href="">Terminos y servicios</a>
+        </div>
+        <div className='footer-right'>
+            <h4>Contactanos</h4>
+            <ul className='footer-info'>
+                <li>Direccion</li>
+                <li>Telefono</li>
+                <li>Email</li>
+            </ul>
+        </div>
+    </footer>
+    <div className='footer-bottom'>
+        <p>&copy; 2026 Grupo Aguapanelita. Todos los derechos reservados</p>
+    </div>
+    </>
+  )
+}
+
+export default Footer
